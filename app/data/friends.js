206bd82b0d396cff7edcee4bf685167d9c8f15be
Richard Selznick <richardselz@gmail.com>
2017-09-25 22:11:21 -0700

    Images Created, Pathed and Routed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friendsArray = [
     {
         name: "One",
-        photo: "123",
+        photo: "static/images/1.png",
         scores: [1,1,1,1,1,1,1,1,1,1]
     },
     {
         name: "Two",
-        photo: "123",
+        photo: "static/images/2.png",
         scores: [2,2,2,2,2,2,2,2,2,2]
     },
     {
         name: "Three",
-        photo: "123",
+        photo: "static/images/3.png",
         scores: [3,3,3,3,3,3,3,3,3,3]
     },
     {
         name: "Four",
-        photo: "123",
+        photo: "static/images/4.png",
         scores: [4,4,4,4,4,4,4,4,4,4]
     },
     {
         name: "Five",
-        photo: "123",
+        photo: "static/images/5.png",
         scores: [5,5,5,5,5,5,5,5,5,5]
     }
 ];
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Require Express and BodyParser
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Set the function Express to the variable app
 var app = express();
@@ -13,6 +14,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+app.use(express.static('public'));
+app.use(express.static('files'));
+
+app.use('/static', express.static(path.join(__dirname, 'app/public')));
 
 //Routers
 require("./app/routing/apiRoutes.js")(app);
